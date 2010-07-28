e348b21d58e7bc2dc2fe73c702225f4ca2c6b60b
Wincent Colaiuta <win@wincent.com>
2010-07-28 21:15:47 +0200

    Switch to BSD license
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- ext/mkdtemp.c
+++ ext/mkdtemp.c
@@ -1,17 +1,26 @@
 /*
-Copyright 2007-2009 Wincent Colaiuta
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
+  Copyright 2007-2010 Wincent Colaiuta. All rights reserved.
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+  Redistribution and use in source and binary forms, with or without
+  modification, are permitted provided that the following conditions are met:
 
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
+  1. Redistributions of source code must retain the above copyright notice,
+     this list of conditions and the following disclaimer.
+  2. Redistributions in binary form must reproduce the above copyright notice,
+     this list of conditions and the following disclaimer in the documentation
+     and/or other materials provided with the distribution.
+
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+  ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+  LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+  POSSIBILITY OF SUCH DAMAGE.
 */
 
 #include <ruby.h>
@@ -24,13 +33,17 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 call-seq:
     Dir.mkdtemp([string])   -> String or nil
 
-This method securely creates temporary directories. It is a wrapper for the mkdtemp() function in the standard C library. It takes
-an optional String parameter as a template describing the desired form of the directory name and overwriting the template in-place;
-if no template is supplied then "/tmp/temp.XXXXXX" is used as a default.
+This method securely creates temporary directories. It is a wrapper for the
+mkdtemp() function in the standard C library. It takes an optional String
+parameter as a template describing the desired form of the directory name and
+overwriting the template in-place; if no template is supplied then
+"/tmp/temp.XXXXXX" is used as a default.
 
-Note that the exact implementation of mkdtemp() may vary depending on the target system. For example, on Mac OS X at the time of
-writing, the man page states that the template may contain "some number" of "Xs" on the end of the string, whereas on Red Hat
-Enterprise Linux it states that the template suffix "must be XXXXXX".
+Note that the exact implementation of mkdtemp() may vary depending on the
+target system. For example, on Mac OS X at the time of writing, the man page
+states that the template may contain "some number" of "Xs" on the end of the
+string, whereas on Red Hat Enterprise Linux it states that the template suffix
+"must be XXXXXX".
 
 */
 static VALUE dir_mkdtemp_m(int argc, VALUE *argv, VALUE self)
